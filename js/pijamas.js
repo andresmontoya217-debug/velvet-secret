ac10feeaf58a11fafa4484ea2dbf5d9a0b32673a
andresmontoya217-debug <andresmontoya217@gmail.com>
2026-08-13 18:15:31 -0500

    Update pijamas.js

--- js/pijamas.js
+++ js/pijamas.js
@@ -32,4 +32,38 @@ window.listaProductos = [
         "../img/Productos/Pijamas/06.jpg"
     ]
 },
+    {
+    nombre: "Pijama Algodón Licrado Ref. 6132",
+
+    referencia: "6132",
+
+    codigo: "PJ105",
+
+    precio: "$75.000",
+
+    categoria: "pijamas",
+
+    nombreCategoria: "Pijamas",
+
+    tallas: ["S/M", "L/XL"],
+
+    descripcion:
+        "Pijama de nueva colección elaborada en algodón licrado grueso, compuesta por pantalón estampado y camiseta.",
+
+    caracteristicas: [
+        "Algodón licrado grueso.",
+        "Conjunto de camiseta y pantalón estampado.",
+        "Tallaje compartido S/M y L/XL.",
+        "Consultar disponibilidad de talla."
+    ],
+
+    imagenes: [
+        "../img/Productos/Pijamas/07.jpg",
+        "../img/Productos/Pijamas/08.jpg",
+        "../img/Productos/Pijamas/09.jpg",
+        "../img/Productos/Pijamas/10.jpg",
+        "../img/Productos/Pijamas/11.jpg",
+        "../img/Productos/Pijamas/12.jpg"
+    ]
+},
 ];
