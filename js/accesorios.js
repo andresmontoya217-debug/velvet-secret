8cbb47fe3bd30904abe3d398fec4e5067e227d1c
velvetsecretco <andresmontoya217@gmail.com>
2026-08-21 23:13:40 -0500

    Update product list in accesorios.js

--- js/accesorios.js
+++ js/accesorios.js
@@ -59,4 +59,242 @@ window.listaProductos = [
         "../img/Productos/Accesorios/3002.jpg"
     ]
 },
+    {
+    nombre: "Potenciador Sexual Femenino Blix",
+
+    referencia: "2989",
+
+    codigo: "2989",
+
+    precio: "$28.000",
+
+    categoria: "potenciadores",
+
+    nombreCategoria: "Accesorios",
+
+    tallas: [],
+
+    descripcion:
+        "Potenciador femenino Blix en presentación de 4 tabletas, elaborado con ingredientes como chontaduro, borojó y polvo de fenogreco.",
+
+    caracteristicas: [
+        "Presentación de 4 tabletas.",
+        "Contiene chontaduro.",
+        "Contiene borojó.",
+        "Contiene polvo de fenogreco.",
+        "Comercializado como potenciador femenino."
+    ],
+
+    imagenes: [
+        "../img/Productos/Accesorios/3003.jpg"
+    ]
+},
+    {
+    nombre: "Bebida Blix 500 ML",
+
+    referencia: "3251",
+
+    codigo: "3251",
+
+    precio: "$80.000",
+
+    categoria: "potenciadores",
+
+    nombreCategoria: "Accesorios",
+
+    tallas: [],
+
+    descripcion:
+        "Bebida Blix de 500 ML a base de agua con extracto de malta, chontaduro y borojó.",
+
+    caracteristicas: [
+        "Presentación de 500 ML.",
+        "A base de agua.",
+        "Contiene extracto de malta.",
+        "Contiene chontaduro.",
+        "Contiene borojó.",
+        "Elaborada con ingredientes de origen natural.",
+        "Según las indicaciones del producto, tomar una copita 30 minutos antes de la actividad."
+    ],
+
+    imagenes: [
+        "../img/Productos/Accesorios/3004.jpg"
+    ]
+},
+   {
+    nombre: "Potenciador Sexual Masculino Blix",
+
+    referencia: "2991",
+
+    codigo: "2991",
+
+    precio: "$28.000",
+
+    categoria: "potenciadores",
+
+    nombreCategoria: "Accesorios",
+
+    tallas: [],
+
+    descripcion:
+        "Potenciador masculino Blix en presentación de 4 tabletas, elaborado con ingredientes como chontaduro, borojó y maca.",
+
+    caracteristicas: [
+        "Presentación de 4 tabletas.",
+        "Contiene chontaduro.",
+        "Contiene borojó.",
+        "Contiene maca.",
+        "Comercializado como potenciador sexual masculino."
+    ],
+
+    imagenes: [
+        "../img/Productos/Accesorios/3005.jpg"
+    ]
+},
+    {
+    nombre: "Campana del Amor con Dados",
+
+    referencia: "4307",
+
+    codigo: "4307",
+
+    precio: "$12.000",
+
+    categoria: "juegos",
+
+    nombreCategoria: "Accesorios",
+
+    tallas: [],
+
+    descripcion:
+        "Campana del Amor acompañada de dados para incorporar un juego diferente y divertido en pareja.",
+
+    caracteristicas: [
+        "Incluye Campana del Amor.",
+        "Incluye dados.",
+        "Dados con posiciones interactivas.",
+        "Ideal para juegos en pareja."
+    ],
+
+    imagenes: [
+        "../img/Productos/Accesorios/3006.jpg"
+    ]
+},
+{
+    nombre: "Juego de 3 Dados",
+
+    referencia: "2349",
+
+    codigo: "2349",
+
+    precio: "$12.000",
+
+    categoria: "juegos",
+
+    nombreCategoria: "Accesorios",
+
+    tallas: [],
+
+    descripcion:
+        "Juego de tres dados diseñado para añadir variedad y diversión a los momentos en pareja.",
+
+    caracteristicas: [
+        "Incluye 3 dados.",
+        "Ideal para juegos en pareja.",
+        "Diseñado para cambiar la rutina.",
+        "Fácil de llevar y utilizar."
+    ],
+
+    imagenes: [
+        "../img/Productos/Accesorios/3007.jpg"
+    ]
+},
+{
+    nombre: "Kit de Amarre para Cama Grey's",
+
+    referencia: "3712",
+
+    codigo: "3712",
+
+    precio: "$32.000",
+
+    categoria: "juegos",
+
+    nombreCategoria: "Accesorios",
+
+    tallas: [],
+
+    descripcion:
+        "Kit de amarre para cama Grey's con sistema de correas de sujeción y esposas ajustables para juegos íntimos en pareja.",
+
+    caracteristicas: [
+        "Sistema de correas de sujeción.",
+        "Esposas ajustables.",
+        "Elaborado en material nylon.",
+        "Diseñado para juegos de inmovilización en pareja.",
+        "Ajustable para mayor comodidad."
+    ],
+
+    imagenes: [
+        "../img/Productos/Accesorios/3008.jpg"
+    ]
+},
+{
+    nombre: "Esposas para Manos y Pies",
+
+    referencia: "3704",
+
+    codigo: "3704",
+
+    precio: "$42.000",
+
+    categoria: "juegos",
+
+    nombreCategoria: "Accesorios",
+
+    tallas: [],
+
+    descripcion:
+        "Sistema de sujeción para manos y pies con conectores metálicos, diseñado para juegos íntimos en pareja.",
+
+    caracteristicas: [
+        "Sistema de sujeción para manos y pies.",
+        "Incluye sujeciones para extremidades superiores e inferiores.",
+        "Conectores metálicos.",
+        "Diseñado para juegos íntimos en pareja."
+    ],
+
+    imagenes: [
+        "../img/Productos/Accesorios/3009.jpg"
+    ]
+},
+{
+    nombre: "Esposas Eróticas con Peluche",
+
+    referencia: "4054",
+
+    codigo: "4054",
+
+    precio: "$18.000",
+
+    categoria: "juegos",
+
+    nombreCategoria: "Accesorios",
+
+    tallas: [],
+
+    descripcion:
+        "Esposas metálicas con recubrimiento de peluche, diseñadas como accesorio para juegos íntimos en pareja.",
+
+    caracteristicas: [
+        "Esposas metálicas.",
+        "Recubrimiento de peluche.",
+        "Diseñadas para juegos en pareja."
+    ],
+
+    imagenes: [
+        "../img/Productos/Accesorios/3010.jpg"
+    ]
+},
+    
 ];
