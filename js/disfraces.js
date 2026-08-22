daf1355074cbe9fb970924f73ca5b96111195a38
velvetsecretco <andresmontoya217@gmail.com>
2026-08-21 23:41:26 -0500

    Update disfraces.js

--- js/disfraces.js
+++ js/disfraces.js
@@ -28,4 +28,437 @@ window.listaProductos = [
         "../img/Productos/Disfraces/1.jpg"
     ]
 },
+    {
+    nombre: "Disfraz Prisionera",
+
+    referencia: "1033",
+
+    codigo: "4230",
+
+    precio: "$66.000",
+
+    categoria: "disfraces",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["Talla única"],
+
+    descripcion:
+        "Disfraz de prisionera en diseño de vestido blanco y negro, disponible en talla única.",
+
+    caracteristicas: [
+        "Diseño de prisionera.",
+        "Tipo vestido.",
+        "Color blanco y negro.",
+        "Talla única."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/2.jpg"
+    ]
+},
+    {
+    nombre: "Disfraz Vestido Cierre",
+
+    referencia: "1137",
+
+    codigo: "4236",
+
+    precio: "$108.000",
+
+    categoria: "disfraces",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["Talla única"],
+
+    descripcion:
+        "Disfraz tipo vestido con cierre, disponible en talla única.",
+
+    caracteristicas: [
+        "Tipo vestido.",
+        "Diseño con cierre.",
+        "Talla única."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/3.jpg"
+    ]
+},
+  {
+    nombre: "Disfraz Coneja",
+
+    referencia: "1351",
+
+    codigo: "4244",
+
+    precio: "$102.000",
+
+    categoria: "disfraces",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["Talla única"],
+
+    descripcion:
+        "Disfraz de coneja en talla única, diseñado para complementar juegos de rol y ocasiones especiales.",
+
+    caracteristicas: [
+        "Diseño de coneja.",
+        "Talla única."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/4.jpg"
+    ]
+},
+ {
+    nombre: "Disfraz Militar",
+
+    referencia: "",
+
+    codigo: "3760",
+
+    precio: "$93.000",
+
+    categoria: "disfraces",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["SM", "ML"],
+
+    descripcion:
+        "Disfraz militar diseñado para juegos de rol, disponible en tallaje SM y ML.",
+
+    caracteristicas: [
+        "Diseño estilo militar.",
+        "Disponible en talla SM.",
+        "Disponible en talla ML.",
+        "Consultar disponibilidad de talla."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/5.jpg"
+    ]
+},
+    {
+    nombre: "Disfraz de Colegiala Japonesa",
+
+    referencia: "",
+
+    codigo: "",
+
+    precio: "$79.000",
+
+    categoria: "disfraces",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["Talla única"],
+
+    descripcion:
+        "Disfraz de colegiala japonesa disponible en diferentes colores y talla única.",
+
+    caracteristicas: [
+        "Diseño de colegiala japonesa.",
+        "Talla única.",
+        "Disponible en diferentes colores.",
+        "Colores azul, rosado y negro.",
+        "Consultar disponibilidad del color deseado."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/6.jpg",
+        "../img/Productos/Disfraces/7.jpg",
+        "../img/Productos/Disfraces/8.jpg",
+        "../img/Productos/Disfraces/9.jpg",
+        "../img/Productos/Disfraces/10.jpg"
+    ]
+},
+  {
+    nombre: "Disfraz Policía",
+
+    referencia: "60074",
+
+    codigo: "4317",
+
+    precio: "$129.000",
+
+    categoria: "disfraces",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["S/M", "L/XL"],
+
+    descripcion:
+        "Disfraz de policía con body y delicados detalles en encaje y transparencias. Incluye accesorios para complementar el conjunto.",
+
+    caracteristicas: [
+        "Body con detalles en encaje.",
+        "Diseño con transparencias.",
+        "Incluye accesorio para la cabeza.",
+        "Incluye esposas.",
+        "Disponible en talla S/M.",
+        "Disponible en talla L/XL.",
+        "Consultar disponibilidad de talla."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/11.jpg"
+    ]
+},
+   {
+    nombre: "Disfraz Enfermera Dana",
+
+    referencia: "",
+
+    codigo: "4314",
+
+    precio: "$80.000",
+
+    categoria: "disfraces",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["Talla única"],
+
+    descripcion:
+        "Disfraz de enfermera Dana en tono rojo, compuesto por body y accesorios para complementar el conjunto.",
+
+    caracteristicas: [
+        "Color rojo.",
+        "Talla única.",
+        "Incluye cofia.",
+        "Incluye gargantilla.",
+        "Incluye body.",
+        "Incluye ligas."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/12.jpg"
+    ]
+}, 
+    {
+    nombre: "Disfraz Piloto",
+
+    referencia: "1463",
+
+    codigo: "4257",
+
+    precio: "$106.000",
+
+    categoria: "disfraces",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["Talla única"],
+
+    descripcion:
+        "Disfraz de piloto en talla única, diseñado para complementar juegos de rol y ocasiones especiales.",
+
+    caracteristicas: [
+        "Diseño de piloto.",
+        "Talla única."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/13.jpg"
+    ]
+},
+   {
+    nombre: "Disfraz Diabla",
+
+    referencia: "1432",
+
+    codigo: "4252",
+
+    precio: "$95.000",
+
+    categoria: "disfraces",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["Talla única"],
+
+    descripcion:
+        "Disfraz de diabla en talla única, diseñado para complementar juegos de rol y ocasiones especiales.",
+
+    caracteristicas: [
+        "Diseño de diabla.",
+        "Talla única."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/14.jpg"
+    ]
+}, 
+{
+    nombre: "Disfraz Enfermera Blanco y Negro",
+
+    referencia: "1055",
+
+    codigo: "4233",
+
+    precio: "$86.000",
+
+    categoria: "disfraces",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["Talla única"],
+
+    descripcion:
+        "Disfraz de enfermera en combinación blanco y negro, disponible en talla única.",
+
+    caracteristicas: [
+        "Diseño de enfermera.",
+        "Color blanco y negro.",
+        "Talla única."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/15.jpg",
+        "../img/Productos/Disfraces/16.jpg"
+    ]
+},
+  {
+    nombre: "Disfraz Novia",
+
+    referencia: "1107",
+
+    codigo: "4234",
+
+    precio: "$80.000",
+
+    categoria: "disfraces",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["Talla única"],
+
+    descripcion:
+        "Disfraz de novia en talla única, diseñado para complementar juegos de rol y ocasiones especiales.",
+
+    caracteristicas: [
+        "Diseño de novia.",
+        "Talla única."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/17.jpg"
+    ]
+},  
+    {
+    nombre: "Disfraz Diabla",
+
+    referencia: "1124",
+
+    codigo: "4235",
+
+    precio: "$79.000",
+
+    categoria: "disfraces",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["Talla única"],
+
+    descripcion:
+        "Disfraz de diabla en talla única, diseñado para complementar juegos de rol y ocasiones especiales.",
+
+    caracteristicas: [
+        "Diseño de diabla.",
+        "Talla única."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/18.jpg"
+    ]
+},
+   {
+    nombre: "Disfraz Colegiala",
+
+    referencia: "1277",
+
+    codigo: "4242",
+
+    precio: "$98.000",
+
+    categoria: "disfraces",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["Talla única"],
+
+    descripcion:
+        "Disfraz de colegiala en talla única, diseñado para complementar juegos de rol y ocasiones especiales.",
+
+    caracteristicas: [
+        "Diseño de colegiala.",
+        "Talla única."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/19.jpg"
+    ]
+}, 
+    {
+    nombre: "Disfraz Monja",
+
+    referencia: "1420",
+
+    codigo: "4247",
+
+    precio: "$96.000",
+
+    categoria: "disfraces",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["Talla única"],
+
+    descripcion:
+        "Disfraz de monja en talla única, diseñado para complementar juegos de rol y ocasiones especiales.",
+
+    caracteristicas: [
+        "Diseño de monja.",
+        "Talla única."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/20.jpg"
+    ]
+},
+    {
+    nombre: "Disfraz Mucama Sexy",
+
+    referencia: "",
+
+    codigo: "4219",
+
+    precio: "$115.000",
+
+    categoria: "disfraces",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["S/M", "L/XL"],
+
+    descripcion:
+        "Disfraz de mucama con top de botones decorativos, falda con delantal incorporado y accesorio para la cabeza.",
+
+    caracteristicas: [
+        "Top con botones decorativos.",
+        "Falda con delantal incorporado.",
+        "Incluye accesorio para la cabeza.",
+        "Materiales suaves y cómodos.",
+        "Disponible en talla S/M.",
+        "Disponible en talla L/XL.",
+        "Consultar disponibilidad de talla."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/21.jpg"
+    ]
+},
 ];
